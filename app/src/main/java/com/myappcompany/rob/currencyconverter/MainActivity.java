e1fa8eabec248bb5f20169ea727c33a0327a6f43
Isanom <we@isanom.com>
2019-01-11 23:30:12 -0700

    Pleaseeeeee

---
+++ app/src/main/java/com/myappcompany/rob/currencyconverter/MainActivity.java
@@ -0,0 +1,35 @@
+package com.myappcompany.rob.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convertCurrency(View view) {
+
+        Log.i("Info", "Button pressed");
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        String amountInPounds = editText.getText().toString();
+
+        double amountInPoundsDouble = Double.parseDouble(amountInPounds);
+
+        double amountInDollarsDouble = amountInPoundsDouble * 1.3;
+
+        String amountInDollarsString = String.format("%.2f", amountInDollarsDouble);
+
+        Toast.makeText(this, "£" + amountInPounds + " is $" + amountInDollarsString, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
